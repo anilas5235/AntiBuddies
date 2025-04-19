557616fdf912592fe88347c594f20be56f0059ff
MalteRFH <maltetorben.fuechsel@rfh-campus.de>
2025-04-19 14:12:21 +0200

    Added 2 test Waves

--- Assets/Project/Scripts/Spawning/WaveHandler.cs
+++ Assets/Project/Scripts/Spawning/WaveHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace Project.Scripts.Spawning
 {
+    [CreateAssetMenu(fileName = "WaveGroup", menuName = "SpawnSystem/WaveGroup")]
     public class WaveGroup : ScriptableObject
     {
         public List<Wave> waves = new();
