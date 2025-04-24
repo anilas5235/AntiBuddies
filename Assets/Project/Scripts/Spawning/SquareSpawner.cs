21423a8c4e64fcfcffa896b2ab3dececa9118e1e
anilas5235 <niklas-borchers@outlook.de>
2025-04-24 18:51:37 +0200

    now generation of ground Details

---
+++ Assets/Project/Scripts/Spawning/SquareSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Project.Scripts.Spawning
+{
+    public class SquareSpawner : MonoBehaviour
+    {
+        [SerializeField] private float size = 5f;
+        [SerializeField] private int spawns = 10;
+        [SerializeField] private GameObject prefabToSpawn;
+
+        private void Awake()
+        {
+            Spawn();
+        }
+
+        private void Spawn()
+        {
+            float halfSize = size / 2;
+            Vector2 basePos = transform.position; // Cache position
+            for (int i = 0; i < spawns; i++)
+            {
+                Vector2 randomPos = new(Random.Range(-halfSize, halfSize), Random.Range(-halfSize, halfSize));
+                Vector2 spawnPosition = basePos + randomPos;
+                Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+            }
+        }
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(1, 0, 0, 1);
+            Gizmos.DrawWireCube(transform.position, new Vector3(size,size,1));
+        }
+#endif
+    }
+}
