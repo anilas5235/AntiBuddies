5bc927491cdcba63c94836982e3c97677b9cd30d
MalteRFH <maltetorben.fuechsel@rfh-campus.de>
2025-05-19 18:09:12 +0200

    Basic Item System

---
+++ Assets/Project/Scripts/ItemSystem/Item.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.ItemSystem
+{
+    [CreateAssetMenu(fileName = "Item", menuName = "ItemSystem/Item")]
+    public class Item : ScriptableObject
+    {
+        [SerializeField] private int cost;
+        [SerializeField] private string description;
+        [SerializeField] private ItemBehaviour[] behaviours;
+    }
+}
---
+++ Assets/Project/Scripts/ItemSystem/ItemStatBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using Project.Scripts.EffectSystem.Components;
+using Project.Scripts.EffectSystem.Effects;
+using UnityEngine;
+
+namespace Project.Scripts.ItemSystem
+{
+    [CreateAssetMenu(fileName = "ItemStatBehaviour", menuName = "ItemSystem/StatBehaviour")]
+    public class ItemStatBehaviour : ItemBehaviour
+    {
+        [SerializeField] private StatData statData;
+        public override void OnAdded(EffectRelay effectRelay)
+        {
+            effectRelay.Apply(statData.GetPackage(null, AlieGroup.Neutral));
+            
+        }
+
+        public override void OnRemoved(EffectRelay effectRelay)
+        {
+            effectRelay.Apply(statData.GetPackage(null, AlieGroup.Neutral).Invert());
+        }
+    }
+}
