5aa3e7a4c6b88c0d3090608dbdfebbe8a42405ea
anilas5235 <niklas-borchers@outlook.de>
2025-06-22 14:44:53 +0200

    Add XML documentation to effect package classes for improved clarity and maintainability

--- Assets/Project/Scripts/EffectSystem/Effects/Data/Package/HealPackage.cs
+++ Assets/Project/Scripts/EffectSystem/Effects/Data/Package/HealPackage.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 namespace Project.Scripts.EffectSystem.Effects.Data.Package
 {
+    /// <summary>
+    /// Represents a package containing all data required to apply a healing effect.
+    /// </summary>
     [Serializable]
     public class HealPackage : EffectPackage
     {
+        /// <summary>
+        /// The type of healing this package represents.
+        /// </summary>
         [SerializeField] private HealType healType;
-
+       
+        /// <param name="amount">The amount of healing.</param>
+        /// <param name="healType">The type of healing.</param>
         public HealPackage(int amount, HealType healType) : base(amount)
         {
             this.healType = healType;
         }
         
+        /// <summary>
+        /// Gets the type of healing.
+        /// </summary>
         public HealType HealType => healType;
     }
 }
--- Assets/Project/Scripts/EffectSystem/Effects/Data/Package/StatPackage.cs
+++ Assets/Project/Scripts/EffectSystem/Effects/Data/Package/StatPackage.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 namespace Project.Scripts.EffectSystem.Effects.Data.Package
 {
+    /// <summary>
+    /// Represents a package containing all data required to apply a stat modification effect.
+    /// </summary>
     [Serializable]
     public class StatPackage : EffectPackage
     {
+        /// <summary>
+        /// The type of stat this package modifies.
+        /// </summary>
         [SerializeField] private StatType statType;
+
+        /// <summary>
+        /// The modification operation to apply to the stat.
+        /// </summary>
         [SerializeField] private StatModification statMod;
 
+        /// <param name="amount">The amount to modify the stat by.</param>
+        /// <param name="statType">The type of stat to modify.</param>
+        /// <param name="statMod">The modification operation.</param>
         public StatPackage(int amount, StatType statType, StatModification statMod) : base(amount)
         {
             this.statType = statType;
             this.statMod = statMod;
         }
 
+        /// <summary>
+        /// Gets the type of stat to modify.
+        /// </summary>
         public StatType StatType => statType;
+
+        /// <summary>
+        /// Gets the modification operation to apply.
+        /// </summary>
         public StatModification StatMod => statMod;
 
+        /// <summary>
+        /// Specifies the type of modification to apply to a stat.
+        /// </summary>
         public enum StatModification
         {
             BaseValue,
@@ -27,6 +50,10 @@ namespace Project.Scripts.EffectSystem.Effects.Data.Package
             MinValue
         }
 
-        public StatPackage Inverse() => new(-Amount, StatType, statMod);
+        /// <summary>
+        /// Creates a new <see cref="StatPackage"/> with the amount inverted.
+        /// </summary>
+        /// <returns>The resulting <see cref="StatPackage"/> with negated amount.</returns>
+        public StatPackage Inverse() => new(-Amount, StatType, statMod); // Negate amount for inverse effect
     }
 }
