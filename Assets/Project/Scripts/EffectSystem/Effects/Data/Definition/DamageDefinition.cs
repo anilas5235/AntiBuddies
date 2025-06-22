e67f63d0340f765eec5ddf586e174ff957a2e6bb
anilas5235 <niklas-borchers@outlook.de>
2025-06-22 14:35:11 +0200

    Refactor effect definitions to use classes instead of records and add XML documentation for improved clarity and maintainability

--- Assets/Project/Scripts/EffectSystem/Effects/Data/Definition/DamageDefinition.cs
+++ Assets/Project/Scripts/EffectSystem/Effects/Data/Definition/DamageDefinition.cs
@@ -8,17 +8,34 @@ using UnityEngine;
 
 namespace Project.Scripts.EffectSystem.Effects.Data.Definition
 {
+    /// <summary>
+    /// Defines the data required to create a damage effect, including type and stat dependencies.
+    /// </summary>
     [Serializable]
-    public record DamageDefinition : EffectDefinition
+    public class DamageDefinition : EffectDefinition
     {
+        /// <summary>
+        /// The type of damage this effect deals.
+        /// </summary>
         [SerializeField] private DamageType damageType;
+
+        /// <summary>
+        /// Additional stat dependencies that can influence the damage calculation.
+        /// </summary>
         [SerializeField] private List<StatDependency> extraStatDeps;
 
+        /// <summary>
+        /// Creates a <see cref="DamagePackage"/> using the definition and provided source/stat context.
+        /// </summary>
+        /// <param name="source">The GameObject that is the source of the damage.</param>
+        /// <param name="statComponent">The stat group to pull the scaling stats from.</param>
+        /// <returns>A new <see cref="DamagePackage"/> instance.</returns>
         public DamagePackage CreatePackage(GameObject source, IStatGroup statComponent)
         {
             int finalAmount = amount;
             if (statComponent != null)
             {
+                // Scale the base amount using the damage type and extra stat dependencies
                 finalAmount = damageType.CreationScale(amount, statComponent, extraStatDeps);
             }
 
--- Assets/Project/Scripts/EffectSystem/Effects/Data/Definition/HealDefinition.cs
+++ Assets/Project/Scripts/EffectSystem/Effects/Data/Definition/HealDefinition.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 namespace Project.Scripts.EffectSystem.Effects.Data.Definition
 {
+    /// <summary>
+    /// Defines the data required to create a healing effect.
+    /// </summary>
     [Serializable]
-    public record HealDefinition : EffectDefinition
+    public class HealDefinition : EffectDefinition
     {
+        /// <summary>
+        /// The type of healing this effect provides.
+        /// </summary>
         [SerializeField] private HealType healType;
 
+        /// <summary>
+        /// Creates a <see cref="HealPackage"/> using the definition.
+        /// </summary>
+        /// <returns>A new <see cref="HealPackage"/> instance.</returns>
         public HealPackage CreatePackage()
         {
             return new HealPackage(amount, healType);
